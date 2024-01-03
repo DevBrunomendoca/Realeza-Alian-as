0a25e4371a86e35d879090c1e74b1a6dde83cc66
Bruno Mendonça <brunomendoncamal@gmail.com>
2024-01-03 17:32:30 -0300

    Update Section About

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,8 +8,8 @@ export default function About() {
     <ContainerAbout id="about">
       <ContentAbout>
         <Title textTitle="QUEM SOMOS NÓS"/>
-        <SecondTitle textTitle="Fundada a partir da inspiração artística e da moda elegante."/>
-        <ParagraphSecond textParagraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit Tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo."/>
+        <SecondTitle textTitle="Atuando desde 2021 no mercado de fabricação de joias"/>
+        <ParagraphSecond textParagraph="A Realeza é uma marca de joias que está revolucionando o mercado desde 2021. Acreditamos apaixonadamente na união dos laços e na conexão entre as almas, e é por isso que cada peça da Realeza é cuidadosamente criada com amor e precisão. Ao escolher uma joia da Realeza, você está trazendo à vida uma história de amor única e especial. Junte-se a nós na celebração do amor e da eternidade através das nossas deslumbrantes joias."/>
       </ContentAbout>
       <BannerAbout>
         <img style={{width:"100%", borderRadius:"10px"}} src="/banner-about.jpg" alt="" />
--- src/components/About/AboutStyle.jsx
+++ src/components/About/AboutStyle.jsx
@@ -7,8 +7,6 @@ align-items: center;
 width: 70%;
 margin: 5rem 15% 13rem;
 gap: 2rem;
-
-
 h2{ 
   color: #928480;
   font-size: 4rem;;
@@ -18,19 +16,15 @@ p{
   max-width: 450px;
 }
 
-@media (max-width:850px) {
+@media (max-width:1250px) {
   flex-direction: column;
-
-  h2, p{ 
-    text-align: center;
-  }
+  align-items: center;
 }
 @media (max-width:600px) {
   width: 90%;
 margin: 3rem 5% 5rem;
 }
 `
-
 export const ContentAbout = styled.div`
 display: flex;
 flex-direction: column;
@@ -38,8 +32,12 @@ justify-content: center;
 align-items: start;
 gap: 2rem;
 max-width: 500px;
-@media (max-width:850px) {
+@media (max-width:1250px) {
   align-items: center;
+
+  h1, h2, p {
+    text-align: center;
+  }
 }
 `
 export const BannerAbout = styled.div`
